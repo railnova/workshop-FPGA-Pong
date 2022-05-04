031dc4b72131b9bb92ce02572177fc6fa5a2d2ce
Charles-Henri Mousset <ch.mousset@gmail.com>
2022-05-04 08:32:42 +0200

    [enh] better rebound logic

--- final/solution.py
+++ final/solution.py
@@ -5,6 +5,7 @@ from amaranth import *
 from amaranth.build import *
 from amaranth_boards.resources import *
 from amaranth_boards.icestick import *
+from amaranth.lib.cdc import FFSynchronizer
 
 # IO definitions for our LED matric and push button extension
 workshop_pcba = [
@@ -63,7 +64,7 @@ class Racket(Elaboratable):
         if player not in [1, 2]:
             raise ValueError("player must be 1 or 2")
         self._player = player
-        self.leds = Signal(8, reset=0b00011000)  # The racket is 2 pixel wide
+        self.pixels = Array(Signal() for _ in range(8))
         self.left = Signal()  # output: set to 1 when the left button is pressed
         self.right = Signal()  # output: set to 1 when the right button is pressed
         self.move_speed=10
@@ -73,19 +74,15 @@ class Racket(Elaboratable):
 
         player = self._player
         if player == 1:
-            m.d.comb += [
-                self.right.eq(~platform.request("button", 1).i),
-                self.left.eq(~platform.request("button", 2).i),
-            ]
+            m.submodules += FFSynchronizer(~platform.request("button", 1).i, self.right)
+            m.submodules += FFSynchronizer(~platform.request("button", 2).i, self.left)
         else:
-            m.d.comb += [
-                self.right.eq(~platform.request("button", 3).i),
-                self.left.eq(~platform.request("button", 4).i),
-            ]
+            m.submodules += FFSynchronizer(~platform.request("button", 3).i, self.right)
+            m.submodules += FFSynchronizer(~platform.request("button", 4).i, self.left)
 
         left = self.left
         right = self.right
-        leds = self.leds
+        leds = Signal(8, reset=0b00011000)  # The racket is 2 pixel wide
 
         # These signals will be set when the rack has to be moved
         move_left = Signal()
@@ -115,6 +112,10 @@ class Racket(Elaboratable):
             with m.If(~leds[0]):  # racket is not on the bottom corner
                 m.d.sync += leds.eq(leds >> 1)
 
+        # present the pixels as an Array for easier access
+        for i in range(8):
+            m.d.comb += self.pixels[i].eq(leds[i])
+
         return m
 
 
@@ -127,7 +128,14 @@ class Ball(Elaboratable):
         self.move_up = Signal() # input: set the vertical movement direction to up
         self.move_down = Signal() # input: set the vertical movement direction to down
         self.reset = Signal()  # input: set to 1 to reset the ball position
-        self.rebound = Signal()  # input: set to 1 to immediately change the horizontal direction of the ball
+        self.two_scored = Signal()  # player 2 scored
+        self.one_scored = Signal()  # player 1 scored
+
+    def set_two_racket_pixels(self, racket_pixels):
+        self._racket_two_pixels = racket_pixels
+
+    def set_one_racket_pixels(self, racket_pixels):
+        self._racket_one_pixels = racket_pixels
 
     def elaborate(self, platform):
         m = Module()
@@ -152,23 +160,28 @@ class Ball(Elaboratable):
         with m.Else():
             m.d.sync += timer.eq(timer - 1)
 
+        # rebound detection
+        rebound = Signal()
+        with m.If((col == 1) & self._racket_two_pixels[row] & ~move_col):
+            m.d.comb += rebound.eq(1)
+        with m.If((col == 6) & self._racket_one_pixels[row] & move_col):
+            m.d.comb += rebound.eq(1)
+
         with m.If(moving):
             # Horizontal movement
-            with m.If(self.rebound):
-                with m.If(col == 0):
-                    m.d.sync += col.eq(1)
-                with m.If(col == 7):
-                    m.d.sync += col.eq(6)    
-                m.d.sync += move_col.eq(~move_col)
-            with m.Elif(timer == 0):
-                m.d.sync += timer.eq(timer.reset),
-                with m.If(move_col):  # ball moving right
-                    with m.If(col != 7):  # ball is on the right: don't move
+            with m.If(timer == 0):
+                with m.If(rebound):
+                    m.d.sync += move_col.eq(~move_col)
+                with m.Elif(col == 0):
+                    m.d.comb += self.two_scored.eq(1)
+                with m.Elif(col == 7):
+                    m.d.comb += self.one_scored.eq(1)
+                with m.Else():
+                    m.d.sync += timer.eq(timer.reset),
+                    with m.If(move_col):  # ball moving towards player 1
                         m.d.sync += col.eq(col + 1)
-                with m.Else():  # ball moving left
-                    with m.If(col!=0):  # ball is on the left: don't move
+                    with m.Else():  # ball moving towards player 2
                         m.d.sync += col.eq(col - 1)
-            
 
             # Vertical movement
             with m.If(timer == 0):
@@ -213,12 +226,12 @@ class Ball(Elaboratable):
                 ]
 
         with m.If(self.reset):
-            with m.If(col[-1]): # right side
+            with m.If(col[-1]): # player one side
                 m.d.sync += [
                     col.eq(6),
                     move_col.eq(0),
                 ]
-            with m.Else():  # left side
+            with m.Else():  # player two side
                 m.d.sync += [
                     col.eq(1),
                     move_col.eq(1),
@@ -232,8 +245,8 @@ class Ball(Elaboratable):
 
 class ScoreUart(Elaboratable):
     def __init__(self):
-        self.score_left = Signal(range(10))
-        self.score_right = Signal(range(10))
+        self.score_two = Signal(range(10))
+        self.score_one = Signal(range(10))
         self.update = Signal()
 
     def elaborate(self, platform):
@@ -243,8 +256,8 @@ class ScoreUart(Elaboratable):
         uart_pins = platform.request("uart")
         uart = m.submodules.uart = AsyncSerialTX(divisor=int(platform.default_clk_frequency // 115200),
                                                  pins=uart_pins)
-        score_left = self.score_left
-        score_right = self.score_right
+        score_two = self.score_two
+        score_one = self.score_one
         update = self.update
 
         with m.FSM(name="score_uart"):
@@ -253,7 +266,7 @@ class ScoreUart(Elaboratable):
                     m.next = "LEFT"
             with m.State("LEFT"):
                 m.d.comb += [
-                    uart.data.eq(ord('0') + score_left),
+                    uart.data.eq(ord('0') + score_two),
                     uart.ack.eq(1),
                 ]
                 with m.If(uart.rdy):
@@ -267,7 +280,7 @@ class ScoreUart(Elaboratable):
                     m.next = "RIGHT"
             with m.State('RIGHT'):
                 m.d.comb += [
-                    uart.data.eq(ord('0') + score_right),
+                    uart.data.eq(ord('0') + score_one),
                     uart.ack.eq(1),
                 ]
                 with m.If(uart.rdy):
@@ -304,61 +317,60 @@ class Pong(Elaboratable):
         ball = m.submodules.ball = Ball()
 
         # build Rackets and display them
-        racket_left = m.submodules.racket_left = Racket(player=2)
-        racket_right = m.submodules.racket_right = Racket()
+        racket_one = m.submodules.racket_one = Racket()
+        racket_two = m.submodules.racket_two = Racket(player=2)
         for i in range(8):
             m.d.comb += [
-                ledm.pixels[0][i].eq(racket_left.leds[i]),
-                ledm.pixels[7][i].eq(racket_right.leds[i]),
+                ledm.pixels[0][i].eq(racket_two.pixels[i]),
+                ledm.pixels[7][i].eq(racket_one.pixels[i]),
             ]
+
+        # pass the racket pixels so that the ball can rebound off it
+        ball.set_one_racket_pixels(racket_one.pixels)
+        ball.set_two_racket_pixels(racket_two.pixels)
 
         # allow the racket to change the ball direction only when it's touching the ball
         with m.If(~ball.col[-1]):
-            m.d.comb += ball.move_up.eq(racket_left.left),
-            m.d.comb += ball.move_down.eq(racket_left.right),
+            m.d.comb += ball.move_up.eq(racket_two.left),
+            m.d.comb += ball.move_down.eq(racket_two.right),
         with m.If(ball.col[-1]):
-            m.d.comb += ball.move_up.eq(racket_right.left),
-            m.d.comb += ball.move_down.eq(racket_right.right),
-
-        # rebound when the ball is on the racket
-        # At this stage, ledm.pixels does not contain the ball, so we can use it to check for ball and racket overlap
-        rebound = Signal()
-        # m.d.comb += rebound.eq(ledm.pixels[ball.col][ball.row])
+            m.d.comb += ball.move_up.eq(racket_one.left),
+            m.d.comb += ball.move_down.eq(racket_one.right),
 
         # Draw the ball
         m.d.comb += ledm.pixels[ball.col][ball.row].eq(1)
 
         # Score
-        score_left = Signal()
-        score_right = Signal()
-        m.d.sync += uart.update.eq(0)  # clears the update signal every clock cycle
-        with m.If(ball.col == 0):
-            with m.If(~rebound):
-                m.d.sync += [
-                    score_left.eq(score_left + 1),
-                    uart.update.eq(1)
-                ]
-                m.d.comb += ball.reset.eq(1),
-        with m.If(ball.col == 7):
-            with m.If(~rebound):
-                m.d.sync += [
-                    score_right.eq(score_right + 1),
-                    uart.update.eq(1)
-                ]
-                m.d.comb += ball.reset.eq(1),
-        with m.If(~platform.request("button", 5)):
-            m.d.comb += [
-                ball.reset.eq(1),
-            ]
+        score_one = Signal(4)
+        score_two = Signal(4)
+        m.d.comb += [
+            uart.score_one.eq(score_one),
+            uart.score_two.eq(score_two),
+        ]
+        m.d.sync += uart.update.eq(0)
+        with m.If(ball.one_scored):
+            m.d.comb += ball.reset.eq(1),
             m.d.sync += [
-                score_left.eq(0),
-                score_right.eq(0),
+                uart.update.eq(1),
+                score_one.eq(score_one + 1),
+            ]
+        with m.If(ball.two_scored):
+            m.d.comb += ball.reset.eq(1),
+            m.d.sync += [
+                uart.update.eq(1),
+                score_two.eq(score_two + 1),
+            ]
+
+        # reset button
+        reset = Signal()
+        m.submodules += FFSynchronizer(~platform.request("button", 5), reset)
+        with m.If(reset):
+            m.d.comb += ball.reset.eq(1),
+            m.d.sync += [
+                score_two.eq(0),
+                score_one.eq(0),
                 uart.update.eq(1)
             ]
-        m.d.comb += [
-            uart.score_right.eq(score_right),
-            uart.score_left.eq(score_left),
-        ]
 
         return m
 
